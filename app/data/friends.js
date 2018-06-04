9abe2e24502ec1276dee501a62f9d48e2b3fd192
Linh Tong <linhthitong@gmail.com>
2018-06-04 16:41:33 -0700

    create server.js, readme and package.json

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var tableFriends = [
+
+
+{
+    "name":"Ryan",
+    "photo":"https://www.popsugar.com/Ryan-Gosling",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  
+];
